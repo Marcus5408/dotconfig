d549468777a48aa92ed20430249e74ca4100d08b
Marcus Jones <marcusjones8119@gmail.com>
2024-06-22 05:20:05 -0400

    init

---
+++ clink/oh-my-posh.lua
@@ -0,0 +1,1 @@
+load(io.popen('oh-my-posh init cmd --config C:/Users/Marcus/.config/ohmyposh/dracula.toml'):read("*a"))()
---
+++ winfetch/config.ps1
@@ -0,0 +1,138 @@
+# ===== WINFETCH CONFIGURATION =====
+
+# $image = "~/winfetch.png"
+# $noimage = $true
+
+# Display image using ASCII characters
+# $ascii = $true
+
+# Set the version of Windows to derive the logo from.
+# $logo = "Windows 10"
+
+# Specify width for image/logo
+# $imgwidth = 24
+
+# Specify minimum alpha value for image pixels to be visible
+# $alphathreshold = 50
+
+# Custom ASCII Art
+# This should be an array of strings, with positive
+# height and width equal to $imgwidth defined above.
+# $CustomAscii = @(
+#     "⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⣦⠀ ⠀"
+#     "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣶⣶⣾⣷⣶⣆⠸⣿⣿⡟⠀ ⠀"
+#     "⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣷⡈⠻⠿⠟⠻⠿⢿⣷⣤⣤⣄⠀⠀ ⠀"
+#     "⠀⠀⠀⠀⠀⠀⠀⣴⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣦⠀ ⠀"
+#     "⠀⠀⠀⢀⣤⣤⡘⢿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⡇ ⠀"
+#     "⠀⠀⠀⣿⣿⣿⡇⢸⣿⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢈⣉⣉⡁ ⠀"
+#     "⠀⠀⠀⠈⠛⠛⢡⣾⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⡇ ⠀"
+#     "⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⠟⠀ ⠀"
+#     "⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⡿⢁⣴⣶⣦⣴⣶⣾⡿⠛⠛⠋⠀⠀ ⠀"
+#     "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠿⠿⢿⡿⠿⠏⢰⣿⣿⣧⠀⠀ ⠀"
+#     "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢿⣿⠟⠀⠀ ⠀"
+# )
+
+# Make the logo blink
+# $blink = $true
+
+# Display all built-in info segments.
+# $all = $true
+
+# Add a custom info line
+# function info_custom_time {
+#     return @{
+#         title = "Time"
+#         content = (Get-Date)
+#     }
+# }
+function info_gpus {
+    return @{
+        title = "GPUs"
+        content = "Intel(R) UHD Graphics, NVIDIA GeForce MX230"
+    }
+}
+
+function info_custom_pkgs {
+    # $scoop = ((scoop list) -replace "Installed apps:\n" | Measure-Object | Select-Object -ExpandProperty Count)
+    # scoop seems to print "Installed apps:" outside of `scoop list` so I can't remove it. hardcoded for now
+    $scoop = 12
+    
+    # $choco = choco list --local-only | Measure-Object | Select-Object -ExpandProperty Count
+    # slow. hardcoded lol
+    $choco = 2
+
+    # $winget = winget list | Measure-Object | Select-Object -ExpandProperty Count
+    # winget is slow, for now its hardcoded
+    $winget = 282
+    
+    # $pip = pip list | Measure-Object | Select-Object -ExpandProperty Count
+    # slow, hardcoded
+    $pip = 216
+    return @{
+        title = "Packages"
+        content = "$scoop (scoop), $choco (choco), $winget (winget), $pip (pip)"
+    }
+}
+
+# Configure which disks are shown
+# $ShowDisks = @("C:", "D:")
+# Show all available disks
+# $ShowDisks = @("*")
+
+# Configure which package managers are shown
+# disabling unused ones will improve speed
+# $ShowPkgs = @("winget", "scoop", "choco")
+
+# Use the following option to specify custom package managers.
+# Create a function with that name as suffix, and which returns
+# the number of packages. Two examples are shown here:
+# $CustomPkgs = @("cargo", "just-install")
+# function info_pkg_cargo {
+#     return (cargo install --list | Where-Object {$_ -like "*:" }).Length
+# }
+# function info_pkg_just-install {
+#     return (just-install list).Length
+# }
+
+# Configure how to show info for levels
+# Default is for text only.
+# 'bar' is for bar only.
+# 'textbar' is for text + bar.
+# 'bartext' is for bar + text.
+$cpustyle = 'bartext'
+$memorystyle = 'bartext'
+$diskstyle = 'bartext'
+$batterystyle = 'bartext'
+
+
+# Remove the '#' from any of the lines in
+# the following to **enable** their output.
+
+@(
+    "title"
+    "dashes"
+    "os"
+    "computer"
+    "kernel"
+    # "motherboard"
+    # "custom_time"  # use custom info line
+    "uptime"
+    # "ps_pkgs"  # takes some time
+    "custom_pkgs"
+    "pwsh"
+    # "resolution"
+    "terminal"
+    # "theme"
+    "cpu"
+    "gpus"
+    "cpu_usage"
+    "memory"
+    "disk"
+    "battery"
+    # "locale"
+    # "weather"
+    # "local_ip"
+    # "public_ip"
+    "blank"
+    "colorbar"
+)
